428a21c0a3d2e8371e59150e7591f826afaaf4bb
Tharusha-bit <tharushanimnathme@gmail.com>
2025-09-26 13:33:23 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://tharusha:Tharusha@cluster0.uft3tiw.mongodb.net/food-del').then(()=>console.log("DB Connected !"));
+    await mongoose.connect('').then(()=>console.log("DB Connected !"));
     
 };
